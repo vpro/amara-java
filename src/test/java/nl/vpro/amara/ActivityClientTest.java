84eabcddd39be22bfcbbb11415fe9bf432aff6d4
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-03-31 17:10:39 +0200

    Added integreation test.

--- src/test/java/nl/vpro/amara/ActivityClientTest.java
+++ src/test/java/nl/vpro/amara/ActivityClientTest.java
@@ -1,11 +1,15 @@
 package nl.vpro.amara;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
 
 import org.junit.Ignore;
 import org.junit.Test;
 
 import nl.vpro.amara.domain.Activity;
+import nl.vpro.amara.domain.Task;
 
 /**
  * @author Michiel Meeuwissen
@@ -21,4 +25,17 @@ public class ActivityClientTest extends AbstractClientsTest {
 
 
     }
+    
+    @Test
+    public void getTasks() {
+        Duration duration = Duration.ofDays(5);
+        System.out.println(duration);
+        Instant after = Instant.now().minus(duration);
+        List<Task> approve = client.teams().getTasks("Approve", after).getTasks();
+        for (Task t: approve) {
+            System.out.println(t);
+        }
+    }
+    
+    
 }
